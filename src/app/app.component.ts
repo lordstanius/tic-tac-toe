94df46fcef2f9ab3ba332106f5c42c49ba3b89c1
Mladen Stanišić <lordstanius@gmail.com>
2019-06-02 07:55:23 +0200

    Cleanup.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
-import { Element } from '@angular/compiler';
-import { MatSelect, MatButton } from '@angular/material';
-import { Field } from './field';
 import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 import { GameService } from './game.service';
 
